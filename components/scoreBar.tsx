226f01cdc8a57fff0a72fcbd27e572055c7e9654
Shehbaz Ali <shahmainaqpa@gmail.com>
2024-11-20 00:05:42 +0500

    :fire: add transion effect & loader

--- components/scoreBar.tsx
+++ components/scoreBar.tsx
@@ -1,28 +1,32 @@
 import React from "react";
-import {ScoreBarProps} from '../dataType/type';
+import { ScoreBarProps } from "../dataType/type";
 
-const ScoreBar: React.FC<ScoreBarProps> = ({ correctRatio, lowestScore, maxScore }) => {
+const ScoreBar: React.FC<ScoreBarProps> = ({
+  correctRatio,
+  lowestScore,
+  maxScore,
+}) => {
   return (
     <div className="w-full mt-10">
       <div className="flex justify-between">
         <p>Score: {maxScore}%</p>
         <p>Max Score: {lowestScore}%</p>
       </div>
-      <div className="h-6 w-full mx-auto border border-gray-300 rounded-sm relative">
+      <div className="h-8 w-full mx-auto border border-slate-400  rounded-md relative overflow-hidden">
         <div
-          className="h-6 bg-gray-700 absolute top-0 left-0 z-20"
+          className="h-full bg-[#6B7280] absolute top-0 left-0 z-20 transition-all ease-in-out duration-500"
           style={{
             width: `${correctRatio}%`,
           }}
         ></div>
         <div
-          className="h-6 bg-gray-300 absolute top-0 left-0 z-10"
+          className="h-full bg-[#9CA3AF] absolute top-0 left-0 z-10 transition-all ease-in-out duration-500"
           style={{
             width: `${lowestScore}%`,
           }}
         ></div>
         <div
-          className="h-6 bg-black absolute top-0 left-0 z-30"
+          className="h-full bg-[#000000] absolute top-0 left-0 z-30 transition-all ease-in-out duration-500"
           style={{
             width: `${maxScore}%`,
           }}
